87fe1c363dfd19b52b555537b4bf2d59d7956935
Dawson Herrington <herrinda@classwork.engr.oregonstate.edu>
2025-05-08 17:48:50 -0700

    fixing git error

--- app.py
+++ app.py
@@ -134,6 +134,134 @@ def Responses():
         if "dbConnection" in locals() and dbConnection:
             dbConnection.close()
 
+# get devices
+@app.route("/Devices", methods=["GET"])
+def Devices():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        query1 = "SELECT * " \
+        "FROM Devices " \
+        
+        devices = db.query(dbConnection, query1).fetchall()
+
+
+        # Query to get all users so we can display them in the dropdown
+        query2 = "SELECT Users.userID, Users.firstName, Users.lastName " \
+        "FROM Users;"
+        users = db.query(dbConnection, query2).fetchall()
+
+
+        # Render the users j2 file, and also send the renderer
+        return render_template(
+            "Devices.j2", 
+            devices=devices,
+            users=users
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+# get deviceServices
+@app.route("/DeviceServices", methods=["GET"])
+def DeviceServices():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        query1 = "SELECT * " \
+        "FROM DeviceServices; " \
+        
+        deviceServices = db.query(dbConnection, query1).fetchall()
+
+        query2 = "SELECT deviceID, deviceName " \
+        "FROM Devices; " \
+
+        devices = db.query(dbConnection, query2).fetchall()
+
+        query3 = "SELECT serviceID, serviceName " \
+        "FROM Services; " \
+
+        services = db.query(dbConnection, query3).fetchall()
+
+        # Render the users j2 file, and also send the renderer
+        return render_template(
+            "DeviceServices.j2", 
+            deviceServices=deviceServices,
+            devices=devices,
+            services=services
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+# get services
+@app.route("/Services", methods=["GET"])
+def Services():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        query1 = "SELECT * " \
+        "FROM Services " \
+        
+        services = db.query(dbConnection, query1).fetchall()
+
+        # Render the users j2 file, and also send the renderer
+        return render_template(
+            "Services.j2", 
+            services=services,
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+# get knownThreats
+@app.route("/KnownThreats", methods=["GET"])
+def KnownThreats():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        query1 = "SELECT * " \
+        "FROM KnownThreats " \
+        
+        knownThreats = db.query(dbConnection, query1).fetchall()
+
+        # Render the users j2 file, and also send the renderer
+        return render_template(
+            "KnownThreats.j2", 
+            knownThreats=knownThreats,
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
 
 
 # ########################################
